f23bab26668b0b23427fdd8f03b93a8a51d41da8
jeffrimko <jeffrimko@gmail.com>
2018-02-12 22:23:09 -0500

    First draft of readme stub. Minor script update.

--- app/_Manage_App.py
+++ app/_Manage_App.py
@@ -30,6 +30,9 @@ def build_app():
     auxly.filesys.makedirs("__output__")
     auxly.shell.call("electron-packager . doctrine2 --out __output__ --electron-version=1.3.0 --overwrite")
 
+def clean_build():
+    auxly.filesys.delete("__output__")
+
 ##==============================================================#
 ## SECTION: Main Body                                           #
 ##==============================================================#
@@ -37,6 +40,7 @@ def build_app():
 if __name__ == '__main__':
     menu = qprompt.Menu()
     menu.add("b", "Build application", build_app)
+    menu.add("c", "Clean build", clean_build)
     menu.add("r", "Run application", run_app)
     menu.add("s", "Set Electron location", set_electron)
     menu.main(loop=True)
