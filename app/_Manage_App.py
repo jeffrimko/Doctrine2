bf6b074122f1e064cd57e46533bd4165ce943bd7
jeffrimko <jeffrimko@gmail.com>
2018-02-12 22:15:49 -0500

    Updated packages.

---
+++ app/_Manage_App.py
@@ -0,0 +1,42 @@
+##==============================================================#
+## SECTION: Imports                                             #
+##==============================================================#
+
+import auxly
+import qprompt
+import os.path as op
+
+##==============================================================#
+## SECTION: Global Definitions                                  #
+##==============================================================#
+
+ELECTRON = r"..\utils\electron_core"
+
+##==============================================================#
+## SECTION: Function Definitions                                #
+##==============================================================#
+
+def set_electron():
+    global ELECTRON
+    ELECTRON = qprompt.ask_str("Path to electron", blank=False)
+
+def run_app():
+    global ELECTRON
+    path = op.normpath(op.join(ELECTRON, "electron.exe"))
+    qprompt.alert(path)
+    auxly.shell.call(f"start {path} .")
+
+def build_app():
+    auxly.filesys.makedirs("__output__")
+    auxly.shell.call("electron-packager . doctrine2 --out __output__ --electron-version=1.3.0 --overwrite")
+
+##==============================================================#
+## SECTION: Main Body                                           #
+##==============================================================#
+
+if __name__ == '__main__':
+    menu = qprompt.Menu()
+    menu.add("b", "Build application", build_app)
+    menu.add("r", "Run application", run_app)
+    menu.add("s", "Set Electron location", set_electron)
+    menu.main(loop=True)
